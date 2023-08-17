1414bcde98d067862985be478bc95c581e6a9caa
wuchiaen1125 <frank11251231@gmail.com>
2023-08-17 12:11:25 +0800

    First Commit - from command line

---
+++ GitForProgrammers/GitForProgrammers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitForProgrammers
+//
+//  Created by 吳佳恩 on 2023/8/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
