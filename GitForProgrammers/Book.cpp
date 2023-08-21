93b370e47d2ac94b1083203dfd67f25f3fd4e0eb
wuchiaen1125 <frank11251231@gmail.com>
2023-08-21 12:24:10 +0800

    (9) Bood.cpp modified (Desktop)

--- GitForProgrammers/Book.cpp
+++ GitForProgrammers/Book.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <list>
 
+
 class Book{
     public:
     void setTitle (std::string bookTitle){
@@ -10,8 +11,18 @@ class Book{
     std::string getTitle (){
         return Title;
     }
+
+    void printPubDate(){
+        std::cout << "The Book is published on " 
+        << pubDay << "/"
+        << pubMonth << "/"
+        << pubYear << ".\n";
+    }
     private:
     std::string Title;
     std::list<std::string> Autours;
+    int pubDay;
+    int pubMonth;
+    int pubYear;
 
 };
