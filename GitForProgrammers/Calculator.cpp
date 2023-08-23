197a3adf6702f4bd6111c0ce881fee9080cf7c0e
wuchiaen1125 <frank11251231@gmail.com>
2023-08-23 16:34:28 +0800

    (12) testing conflict (wuchiaen)

--- GitForProgrammers/Calculator.cpp
+++ GitForProgrammers/Calculator.cpp
@@ -7,3 +7,6 @@ class Calculator {
     }
 
 };
+int main(){
+    std::string comment1 = "I am adding a line in the Calculator branch. Later, I will create another branch \"Calculator1\" to show conflict during merge.";
+}
