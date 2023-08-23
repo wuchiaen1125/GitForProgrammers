b45ea94798cb66472d28ccd93b2c0314836692e1
wuchiaen1125 <frank11251231@gmail.com>
2023-08-23 17:15:31 +0800

    (15) comments about resolving conflicts)

--- GitForProgrammers/Calculator.cpp
+++ GitForProgrammers/Calculator.cpp
@@ -11,4 +11,7 @@ class Calculator {
 int main(){
     std::string comment1 = "I am adding a line in the Calculator branch. Later, I will create another branch \"Calculator1\" to show conflict during merge.";
     std::string comment2 = "the above part are copied from branch Calulator. This line is added in branch Calculator1";
+    //when conflicts occurs, click "resolve in merge editor" and decide which version you want it remain, then "complete merge" 
+    //in this attempt of showing conflict during merging, I remain the one in branch Calculator1
+    //the three comment lines are added after the completion of merge (i.e. afterwards) 
 }
