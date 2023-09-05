8bc4db095d0ae77acf531ccea1dce13cd0c63810
wuchiaen1125 <frank11251231@gmail.com>
2023-09-05 13:20:06 +0800

    (19) amending main s.t. main is not the main used as base for Book (wuchiaen)

--- GitForProgrammers/GitForProgrammers/main.cpp
+++ GitForProgrammers/GitForProgrammers/main.cpp
@@ -11,5 +11,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n" << "I am adding a new line.\n";
     std::cout << "this line is added (in /Users/wuchiaen/Documents/Desktop/GitForProgrammers) to show the staging area" << std::endl;
+    std::cout << "\n\nwriting a new line to make the current \"main\" have one more commit than the original \"main\" which is used as the base for branch \"Book\" \n" 
+    << "this is to do the command \"rebase\" 如果沒有rebase，在合併branch into main時會自動多產生一個commit \"Merge branch \'...\'\"" << std::endl; 
     return 0;
 }
