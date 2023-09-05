6da1e840545cf8b7b863e3795b5da082ab293993
wuchiaen1125 <frank11251231@gmail.com>
2023-09-05 13:03:09 +0800

    (17)Book.cpp (Member Function) setAuthors added(wuchiaen)

--- GitForProgrammers/Book.cpp
+++ GitForProgrammers/Book.cpp
@@ -11,6 +11,17 @@ class Book{
     std::string getTitle (){
         return Title;
     }
+    void setAuthors(){
+        std::cout << "please enter the name of author or \"end\" to end" << std::endl;
+        std::string name;
+        std::cin >> name;
+        while (name != "end"){
+            Authors.push_back(name);
+            std::cout << "please enter the name of another author or \"end\" to end" << std::endl;
+            std::cin >> name;
+        }
+        if (name == "end") std::cout << "finished" << std::endl;
+    }
     void setPubDay (int day){
         if (day > 0 && day <= 31) pubDay = day;
         else {
@@ -26,7 +37,7 @@ class Book{
     }
     private:
     std::string Title;
-    std::list<std::string> Autours;
+    std::list<std::string> Authors;
     int pubDay;
     int pubMonth;
     int pubYear;
