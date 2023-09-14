2d91913d551b35d886933ce06e7f5a0609593921
wuchiaen1125 <frank11251231@gmail.com>
2023-09-15 05:35:51 +0800

    (21) problem during push

--- GitForProgrammers/GitForProgrammers/main.cpp
+++ GitForProgrammers/GitForProgrammers/main.cpp
@@ -16,5 +16,8 @@ int main(int argc, const char * argv[]) {
     std::cout << "noticing commit 17 is ahead of 19. 17 was after 19 in the first place, after rebasing a new commit id is given, so it becomes ahead of 19\n" 
     << "also noticing that after merge there is no Git-generating commit\n"
     << "also commit 18 (pulling the new main to branch) is omitted " << std::endl;
+    std::cout << "push時出現：\nremote: Invalid username or password.\n" 
+    << "fatal: Authentication failed for...。可能是因為token失效了\n"
+    << "問題是出在mirror副本，不能push，但正本可以。\n";
     return 0;
 }
